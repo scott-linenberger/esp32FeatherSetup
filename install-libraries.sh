cb31745d579113313d80544b600995acbf25e4c7
Scott Linenberger <scott.linenberger@vml.com>
2018-03-16 12:04:25 -0500

    create script for downloading support librarires

---
+++ install-libraries.sh
@@ -0,0 +1,21 @@
+cd ~/Documents/Arduino/libraries
+
+# download the iot status lib
+echo -e "\nCloning IotStatusIndicator..."
+git clone https://github.com/scott-linenberger/iot_status_indicator.git
+
+# download the adafruit NeoPixel lib
+echo -e "\nCloning Adafruit NeoPixel..."
+git clone https://github.com/adafruit/Adafruit_NeoPixel.git
+
+# download the Adafruit IO libraries
+echo -e "\nCloning Adafruit IO..."
+git clone https://github.com/adafruit/Adafruit_IO_Arduino.git
+
+#download the Adafruit MQTT libraries
+echo -e "\nCloning Adafruit MQTT..."
+git clone https://github.com/adafruit/Adafruit_MQTT_Library.git
+
+#download the Arduino HTTP client
+echo -e "\nDownloading the Arduino HTTP client..."
+git clone https://github.com/arduino-libraries/ArduinoHttpClient.git
